0736be97fde53a171e378ed2ef6598175964d920
shanks-t <shanksleslietrey@gmail.com>
2024-06-01 20:09:17 -0500

    trained model with augmentation, needs more testing

--- scripts/copy_data.sh
+++ scripts/copy_data.sh
@@ -1,6 +1,12 @@
-cp /Users/treyshanks/Desktop/lebron_2/test/images/* /Users/treyshanks/data_science/Court_Vision/agg_lebron_data/test/images/ && \
-cp /Users/treyshanks/Desktop/lebron_2/test/labels/* /Users/treyshanks/data_science/Court_Vision/agg_lebron_data/test/labels/ && \
-cp /Users/treyshanks/Desktop/lebron_2/train/images/* /Users/treyshanks/data_science/Court_Vision/agg_lebron_data/train/images/ && \
-cp /Users/treyshanks/Desktop/lebron_2/train/labels/* /Users/treyshanks/data_science/Court_Vision/agg_lebron_data/train/labels/ && \
-cp /Users/treyshanks/Desktop/lebron_2/valid/images/* /Users/treyshanks/data_science/Court_Vision/agg_lebron_data/valid/images/ && \
-cp /Users/treyshanks/Desktop/lebron_2/valid/labels/* /Users/treyshanks/data_science/Court_Vision/agg_lebron_data/valid/labels/ 
+cp /Users/treyshanks/Desktop/lebron_2/test/images/* /Users/treyshanks/Desktop/agg_lebron/test/images/ && \
+cp /Users/treyshanks/Desktop/lebron_2/test/labels/* /Users/treyshanks/Desktop/agg_lebron/test/labels/ && \
+cp /Users/treyshanks/Desktop/lebron_2/train/images/* /Users/treyshanks/Desktop/agg_lebron/train/images/ && \
+cp /Users/treyshanks/Desktop/lebron_2/train/labels/* /Users/treyshanks/Desktop/agg_lebron/train/labels/ && \
+cp /Users/treyshanks/Desktop/lebron_2/valid/images/* /Users/treyshanks/Desktop/agg_lebron/valid/images/ && \
+cp /Users/treyshanks/Desktop/lebron_2/valid/labels/* /Users/treyshanks/Desktop/agg_lebron/valid/labels/ && \
+cp /Users/treyshanks/Desktop/lebron_1/test/images/* /Users/treyshanks/Desktop/agg_lebron/test/images/ && \
+cp /Users/treyshanks/Desktop/lebron_1/test/labels/* /Users/treyshanks/Desktop/agg_lebron/test/labels/ && \
+cp /Users/treyshanks/Desktop/lebron_1/train/images/* /Users/treyshanks/Desktop/agg_lebron/train/images/ && \
+cp /Users/treyshanks/Desktop/lebron_1/train/labels/* /Users/treyshanks/Desktop/agg_lebron/train/labels/ && \
+cp /Users/treyshanks/Desktop/lebron_1/valid/images/* /Users/treyshanks/Desktop/agg_lebron/valid/images/ && \
+cp /Users/treyshanks/Desktop/lebron_1/valid/labels/* /Users/treyshanks/Desktop/agg_lebron/valid/labels/ 
